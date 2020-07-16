99c5747b0e4241194754d2adb25df8557cc85c6e
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-07-16 11:03:37 -0300

    feat(react): add `<Icon />` component

--- examples/react/src/App/index.js
+++ examples/react/src/App/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { Provider, Button } from "@naturacosmeticos/natds-web";
+import {Provider, Button, Icon} from "@naturacosmeticos/natds-web";
 
 export const App = () => (
     <Provider>
-        <Button color={"primary"} variant={"contained"}>Button</Button>
+        <Button color={"primary"} variant={"contained"}>
+            <Icon name={"filled-action-add"} size={"tiny"} />
+            Button
+        </Button>
     </Provider>
 )
 
